763176c2c46851ec76258b5cfb978058776f6824
Payal Sasmal <sasmalpayal@gmail.com>
2022-04-24 17:13:38 +0530

    first commit

---
+++ foodapp/foodApp/models.py
@@ -0,0 +1,11 @@
+import email
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+
+class RestaurantServiceRegistration(models.Model):
+    emailId = models.EmailField(max_length=255, unique=True, db_index=True)
+    name = models.CharField()
+
+
