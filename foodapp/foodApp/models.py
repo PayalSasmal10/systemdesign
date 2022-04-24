b02a3ffe123c6378e8cfc69af987004a5a816279
Payal Sasmal <sasmalpayal@gmail.com>
2022-04-24 17:50:14 +0530

    updated views.py

--- foodapp/foodApp/models.py
+++ foodapp/foodApp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class RestaurantServiceRegistration(models.Model):
     emailId = models.EmailField(max_length=255, unique=True, db_index=True)
-    name = models.CharField()
+    name = models.CharField(max_length=255)
+    
 
 
--- foodapp/foodApp/views.py
+++ foodapp/foodApp/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from abc import abstractmethod
 
 # Create your views here.
 class RestaurantService:
-    pass
+    @abstractmethod
+    def SignUp(self,email, name, password, contactno):
+        pass
+    
+    @abstractmethod
+    def restaurantSignIn(self, email, password):
+        pass
+
+    @abstractmethod
+    def restaurantAdd(self, restaurantname, foodmenu):
+        
+        @abstractmethod
+        def foodmenu(self, foodname, description, picture):
+            pass
+
+
+class CustomerService:
+
+    @abstractmethod
+    def SignUp(self, first_name, second_name, email, password):
+        pass
+    
+    @abstractmethod
+    def SignIn(self, email, password):
+        pass
+    
+
+
+
+
+
+
+
